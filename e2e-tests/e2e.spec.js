2813b48ee2e5e4ed16c9a067a75175ba74152dab
Tsirbunen <tsirbunen@gmail.com>
2024-08-08 08:15:54 +0300

    Add Playwright tests

---
+++ e2e-tests/e2e.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    // The base url is defined in the config file
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
