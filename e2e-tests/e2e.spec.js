fb99e1fa3470cd2ccc7006ad6b6a4e39071caa48
Tsirbunen <tsirbunen@gmail.com>
2024-08-08 08:39:21 +0300

    Add an e2e test

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -8,4 +8,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('one can navigate to the page of a particular Pokemon', async ({ page }) => {
+    // The base url is defined in the config file
+    await page.goto('')
+    await page.click('text=ivysaur')
+    await expect(page.getByText('Chlorophyll')).toBeVisible()
+  })
 })
