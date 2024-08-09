78ef00adeebf59696765654c81070b153713243f
Tsirbunen <tsirbunen@gmail.com>
2024-08-09 07:31:45 +0300

    Add automatic version bump and tagging

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('4')
+  res.send('5')
 })
 
 app.get('/health', (req, res) => {
